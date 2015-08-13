f536e98917cff6a3a274d765c01bce1085131d23
Josh Sephton <josh.sephton@gmail.com>
2015-08-13 19:01:33 +0100

    Can return shortest path when unconnected vertices

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -175,4 +175,21 @@ RSpec.describe Graph do
 
 	end
 
+	it "can return shortest path when unconnected vertices" do
+
+		shortest_path = Hash.new
+		shortest_path['A'] = 0;
+		shortest_path['B'] = 5;
+		shortest_path['C'] = 8;
+		shortest_path['D'] = Graph::MAX;
+
+		graph = Graph.new
+		graph.add_vertex('D')
+		graph.add_edge('A', 'B', 5)
+		graph.add_edge('B', 'C', 4)
+		graph.add_edge('A', 'C', 8)
+		expect(graph.shortest_path('A')).to eq shortest_path
+
+	end
+
 end
