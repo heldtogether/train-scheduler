22f8cd7c5d78420d09dc232ecc0bc10f735caa26
Josh Sephton <josh.sephton@gmail.com>
2015-08-15 18:14:10 +0100

    Update main program to use new path explorer checkers

--- main.rb
+++ main.rb
@@ -2,6 +2,9 @@ require './lib/graph'
 require './lib/path_measurer'
 require './lib/path_searcher'
 require './lib/path_explorer'
+require './lib/exact_path_length_checker'
+require './lib/max_path_distance_checker'
+require './lib/max_path_length_checker'
 
 edges = []
 
@@ -12,9 +15,18 @@ end
 
 
 graph = Graph.new
+
 path_measurer = PathMeasurer.new(graph)
+
 path_searcher = PathSearcher.new(graph)
-path_explorer = PathExplorer.new(graph)
+
+exact_path_length_checker = ExactPathLengthChecker.new(graph)
+max_path_length_checker = MaxPathLengthChecker.new(graph)
+max_path_distance_checker = MaxPathDistanceChecker.new(graph)
+
+exact_path_length_path_explorer = PathExplorer.new(graph, exact_path_length_checker)
+max_path_length_path_explorer = PathExplorer.new(graph, max_path_length_checker)
+max_path_distance_path_explorer = PathExplorer.new(graph, max_path_distance_checker)
 
 edges.each do | edge |
 	graph.add_edge(edge[0], edge[1], edge[2..-1].to_i)
@@ -35,14 +47,14 @@ puts 'Output #4: ' + path_measurer.distance(['A', 'E', 'B', 'C', 'D']).to_s
 
 # 5. The distance of the route A-E-D.
 puts 'Output #5: ' + path_measurer.distance(['A', 'E', 'D']).to_s
-
+#
 # 6. The number of trips starting at C and ending at
 # C with a maximum of 3 stops.
-puts 'Output #6: ' + path_explorer.num_trips('C', 'C', 3, Graph::MaxStops).to_s
+puts 'Output #6: ' + max_path_length_path_explorer.explore('C', 'C', 3).count.to_s
 
 # 7. The number of trips starting at A and ending at
 # C with exactly 4 stops.
-puts 'Output #7: ' + path_explorer.num_trips('A', 'C', 4, Graph::ExactStops).to_s
+puts 'Output #7: ' + exact_path_length_path_explorer.explore('A', 'C', 4).count.to_s
 
 # 8. The length of the shortest route (in terms of
 # distance to travel) from A to C.
@@ -54,4 +66,4 @@ puts 'Output #9: ' + path_searcher.shortest_path('B', 'B').to_s
 
 # 10. The number of different routes from C to C with
 # a distance of less than 30.
-puts 'Output #10: ' + path_explorer.num_trips('C', 'C', 30, Graph::MaxDistance).to_s
+puts 'Output #10: ' + max_path_distance_path_explorer.explore('C', 'C', 30).to_s
