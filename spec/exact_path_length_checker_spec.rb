bd9eb30ef8e362f8a5900010cd050306e48b9dac
Josh Sephton <josh.sephton@gmail.com>
2015-08-15 15:17:25 +0100

    Doesn't accept paths that are not the exact length

--- spec/exact_path_length_checker_spec.rb
+++ spec/exact_path_length_checker_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe ExactPathLengthChecker do
 
 	end
 
+	it "doesn't accept paths that are not the exact length" do
+
+		graph = Graph.new
+
+		checker = ExactPathLengthChecker.new(graph, 1)
+
+		expect(checker.valid_path?(['A', 'B'])).to eq false
+
+	end
+
 end
