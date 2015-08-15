ff4607d1fcf5769957d959314d80413f96abc79d
Josh Sephton <josh.sephton@gmail.com>
2015-08-15 12:32:46 +0100

    Can find all vertices in 0-edge graph

--- lib/path_explorer.rb
+++ lib/path_explorer.rb
@@ -14,7 +14,30 @@ class PathExplorer
 
 	def explore (tail)
 
+		vertices_to_explore = [tail]
+		discovered_vertices = []
 
+		while vertex = vertices_to_explore.pop
+
+			if ! discovered_vertices.include? vertex
+
+				discovered_vertices.push vertex
+
+				if @graph.edges.has_key? vertex
+
+					@graph.edges[vertex].each do | vertex, distance |
+
+						vertices_to_explore.push vertex
+
+					end
+
+				end
+
+			end
+
+		end
+
+		return discovered_vertices
 
 	end
 
--- spec/path_explorer_spec.rb
+++ spec/path_explorer_spec.rb
@@ -4,6 +4,15 @@ require 'path_explorer'
 
 RSpec.describe PathExplorer do
 
-	# 
+	it "can find all vertices in 0-edge graph" do
+
+		graph = Graph.new
+
+		path_explorer = PathExplorer.new(graph)
+
+		expect(path_explorer.explore('A')).to eq ['A']
+
+	end
+
 
 end
