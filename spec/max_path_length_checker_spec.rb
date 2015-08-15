4e4fc12baba04123f3f2d683cca64dd3f5602e47
Josh Sephton <josh.sephton@gmail.com>
2015-08-15 15:24:56 +0100

    Max path length checker declares finished exploring correctly

--- spec/max_path_length_checker_spec.rb
+++ spec/max_path_length_checker_spec.rb
@@ -33,4 +33,25 @@ RSpec.describe MaxPathLengthChecker do
 
 	end
 
+	it "doesn't declare finished exploring by default" do
+
+		graph = Graph.new
+
+		checker = MaxPathLengthChecker.new(graph, 1)
+
+		expect(checker.finished_exploring?()).to eq false
+
+	end
+
+	it "does declare finished exploring once exact path length reached" do
+
+		graph = Graph.new
+
+		checker = MaxPathLengthChecker.new(graph, 1)
+		checker.valid_path?(['A', 'B'])
+
+		expect(checker.finished_exploring?()).to eq true
+
+	end
+
 end
